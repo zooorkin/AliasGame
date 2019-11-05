e733a1412e7dc62500ba0ce666a79a815f95c89f
zooorkin <zooorkin@icloud.com>
2019-11-05 18:38:07 +0300

    Создан файл проекта

---
+++ GuessTheWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuessTheWorld
+//
+//  Created by Андрей Зорькин on 05/11/2019.
+//  Copyright © 2019 Андрей Зорькин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
